72c12ec32c35837cab0f787accbbc6bd82d8cd05
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2023-12-03 00:40:23 +0300

    Задания готовы

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b**2 - 4*a*c;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b/(2*a)];
+  } else {
+    arr = [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100;
+  let loanBase = amount - contribution;
+  let monthlyRate = percent / 12;
+  let monthlyPayment = loanBase * (monthlyRate + (monthlyRate / (((1 + monthlyRate)**countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  return totalAmount.toFixed(2);
 }
+
+let result = Number(calculateTotalMortgage(10, 0, 50000, 12));
+
+console.log(result);
