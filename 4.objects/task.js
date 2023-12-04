78877beda633ab45fb1daad4cf30961f9f5928a5
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2023-12-04 20:45:10 +0300

    Внесены правки

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,30 +4,28 @@ function Student(name, gender, age) {
     this.age = age;
     this.marks = [];
   }
-
-  let student1 = new Student("Василиса", "женский", 19);
-  let student2 = new Student("Артём", "мужской", 25);
   
   Student.prototype.setSubject = function (subjectName) {
-    this.subjectName = subjectName;
+    this.subject = subjectName;
   }
   
   Student.prototype.addMarks = function (...marks) {
-    if (!this.reason) {
+    if (!this.marks || this.marks.length === 0) {
       this.marks = [...marks]; 
+    } else {
+        this.marks.push(...marks);
     }
   }
   
   Student.prototype.getAverage = function () {
     if (!this.marks || this.marks.length === 0) {
       return 0;
-    } else {
-      return this.marks.reduce(( a, v ) => a + v, 0 ) / this.marks.length;
     }
+    return this.marks.reduce((previousMark, nextMark) => previousMark + nextMark, 0 ) / this.marks.length;
   }
   
   Student.prototype.exclude = function (reason) {
     delete this.marks;
     delete this.subjectName;
-    this.reason = reason;
+    this.excluded = reason;
   }
