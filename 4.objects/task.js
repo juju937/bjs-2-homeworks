b404507474e74f5f2e3d404945e4eb68bcbe1746
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2023-12-04 05:27:38 +0300

    Задания выполнены

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subjectName = subjectName;
+  }
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.addMarks = function (...marks) {
+    if (!this.reason) {
+      this.marks = [...marks]; 
+    }
+  }
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.getAverage = function () {
+    if (!this.marks || this.marks.length === 0) {
+      return 0;
+    } else {
+      return this.marks.reduce(( a, v ) => a + v, 0 ) / this.marks.length;
+    }
+  }
   
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.marks;
+    delete this.subjectName;
+    this.reason = reason;
+  }
