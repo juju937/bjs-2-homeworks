4418dbfecbbf20fb4aaaa0dadad0bb493bc4eaab
Elizabeth <141666125+juju937@users.noreply.github.com>
2024-01-24 17:40:46 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length) {
+    return arr1.every((element, index) => {
+      if (element === arr2[index]) {
+        return true;
+      }
+      return false;
+    });
+  }
+  return false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const usersFound = people.filter((user) => user.gender === gender);
+    if (usersFound.length === 0 || users.length === 0) {
+        return 0;
+        }
+  return usersFound.reduce((prev, user) => prev + user.age, 0) / usersFound.length;
 }
